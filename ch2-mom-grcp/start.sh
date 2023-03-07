c0876ce97b707a74066c5dfe8d3eadca16b7cc02
Ubuntu <ubuntu@ip-172-31-12-14.ec2.internal>
2023-03-07 00:29:30 +0000

    start script update

--- ch2-mom-grcp/start.sh
+++ ch2-mom-grcp/start.sh
@@ -4,9 +4,9 @@ sudo systemctl start rabbitmq-server
 
 sudo service nginx restart
 
-python3 /home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/microservices/rpc_server.py /home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/microservices/config.json &
+python3 /home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/microservices/rpc_server.py 
 
-python3 /home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/microservices/grpc_server.py /home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/microservices/config.json &
+python3 /home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/microservices/grpc_server.py
 
 cd /home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/
 
