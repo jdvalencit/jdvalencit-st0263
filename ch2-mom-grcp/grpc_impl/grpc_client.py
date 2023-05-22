a8b4259cd9d6a6c1c23f9a6ebeba8a70e0edf48b
jdvalencit <jdvalencit@eafit.edu.co>
2023-05-22 17:52:38 -0500

    Map reduce lab initial setup

--- ch2-mom-grcp/grpc_impl/grpc_client.py
+++ ch2-mom-grcp/grpc_impl/grpc_client.py
@@ -5,16 +5,18 @@ from grpc_impl import grpc_service_pb2_grpc
 
 config = json.load(open(r"/home/ubuntu/jdvalencit-st0263/ch2-mom-grcp/config.json", 'r'))
 
+
 def list(stub):
     response = stub.list(grpc_service_pb2.list_req())
-    res = {"method":response.method,"list":"".join(response.files)}
+    res = {"method": response.method, "list": "".join(response.files)}
     return json.dumps(res)
+
 
 def search(stub, file):
     response = stub.search(grpc_service_pb2.search_req(file_name=file))
     res = {"method":response.method,"file":file,"status":response.status,"path":response.path}
     return json.dumps(res)
-    
+
 
 def grpc_service(req):
     with grpc.insecure_channel(f"{config['grpc_client_host']}:{config['grpc_client_port']}") as channel:
